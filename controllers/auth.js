2d258e2ff8b22a1cf7513dc49170fc0b7d7cb37b
Jackson Rodríguez <67988870+jacksonr20@users.noreply.github.com>
2023-03-31 13:41:01 -0500

    Merge Google Login from Release/v3.0.0 to develop (#8)
    * feat: middlewares were set
    
    * feat: Google login added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import { request, response } from 'express';
 import { User } from '../models/user.js';
-import { generateJWT } from '../helpers/generateJWT.js';
+import { generateJWT, googleVerify } from '../helpers/index.js';
 
 const login = async (req = request, res = response) => {
   const { email, password } = req.body;
@@ -41,4 +41,48 @@ const login = async (req = request, res = response) => {
   }
 };
 
-export { login };
+const googleSignIn = async (req = request, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { name, picture, email } = await googleVerify(id_token);
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      const data = {
+        name,
+        email,
+        password: ':P',
+        picture,
+        google: true,
+        role: 'USER_ROLE',
+      };
+
+      user = new User(data);
+      await user.save();
+    }
+
+    if (!user.status) {
+      return res.status(401).json({
+        msg: 'Speak to the administrator, User blocked',
+      });
+    }
+
+    const token = await generateJWT(user.id);
+
+    res.json({
+      user,
+      token,
+      id_token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      ok: false,
+      msg: 'Token could not be verified',
+    });
+  }
+};
+
+export { login, googleSignIn };
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
-import { login } from '../controllers/auth.js';
+import { login, googleSignIn } from '../controllers/auth.js';
 import { validateFields } from '../middlewares/validateFields.js';
 
 const authRouter = Router();
@@ -13,6 +13,12 @@ authRouter.post(
     validateFields,
   ],
   login
+);
+
+authRouter.post(
+  '/google',
+  [check('id_token', 'id_token is required').notEmpty(), validateFields],
+  googleSignIn
 );
 
 export { authRouter };
